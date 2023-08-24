7375215231e6908a93707ed7670be6dd96cf3d6f
dev-rome <haynesjerome@gmail.com>
2023-08-24 19:23:14 -0400

    add home page layout

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+// import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <section>
+      <h1 className="head-text">Home</h1>
+    </section>
+  );
+}
+
+{
+  /* <UserButton afterSignOutUrl="/"/> */
+}
