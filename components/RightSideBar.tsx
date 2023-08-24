4e0508def4b4e559ab779dba38640324984e4ebe
dev-rome <haynesjerome@gmail.com>
2023-08-24 19:20:12 -0400

    add right side bar

--- components/RightSideBar.tsx
+++ components/RightSideBar.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const RightSideBar = () => {
   return (
-    <div>RightSideBar</div>
-  )
-}
+    <aside className="custom-scrollbar rightsidebar">
+      <div className="flex flex-col flex-1 justify-start">
+        <h3 className="text-heading4-medium text-light-1">
+          Suggested Communities
+        </h3>
+      </div>
+      <div className="flex flex-col flex-1 justify-start">
+        <h3 className="text-heading4-medium text-light-1">Suggested Users</h3>
+      </div>
+    </aside>
+  );
+};
 
-export default RightSideBar
+export default RightSideBar;
