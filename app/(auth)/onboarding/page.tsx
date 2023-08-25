989b0b6523d2d35ac1fc74034d4f91fb57a02dde
dev-rome <haynesjerome@gmail.com>
2023-08-25 07:06:50 -0400

    add userData and import account profile

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,10 +1,31 @@
+import { currentUser } from "@clerk/nextjs";
 
+import AccountProfile from "@/components/forms/AccountProfile";
 
 export default async function page() {
-  return (
-    <section>
-        <h1 className="head-text">Onboarding</h1>
-    </section>
-  )
-}
+  const user = await currentUser();
 
+  const userInfo = {};
+
+  const userData = {
+    id: user?.id,
+    objectId: userInfo?._id,
+    username: userInfo?.username || user?.username,
+    name: userInfo?.name || user?.firstName || "",
+    bio: userInfo?.bio || "",
+    image: userInfo?.image || user?.imageUrl,
+  };
+
+  return (
+    <section className="max-w-3xl mx-auto flex flex-col justify-start px-10 py-20">
+      <h1 className="head-text">Onboarding</h1>
+      <p className="mt-3 text-base-regular text-light-2">
+        Complete your profile now to use Threads
+      </p>
+
+      <div className="mt-9 bg-dark-2 p-10">
+        <AccountProfile user={userData} btnTitle="Continue" />
+      </div>
+    </section>
+  );
+}
