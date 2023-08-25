5cd896e3cf649d925570b6bce0143f7d7cdc5135
dev-rome <haynesjerome@gmail.com>
2023-08-25 10:20:12 -0400

    add user profile check and thread creation page

--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -1,7 +1,23 @@
-export default function page() {
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { fetchUser } from "@/lib/actions/user.actions";
+
+import PostThread from "@/components/forms/PostThread";
+
+export default async function page() {
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+
+  if (!userInfo?.onboarded) redirect("/onboarding");
   return (
     <section>
-      <h1>Hello</h1>
+      <h1 className="head-text">Create Thread</h1>
+
+      <PostThread userId={userInfo._id} />
     </section>
   );
 }
