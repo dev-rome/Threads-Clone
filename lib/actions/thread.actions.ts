e4d967b4b56d77ad7a498c10c73f0d9c89b946f1
dev-rome <haynesjerome@gmail.com>
2023-08-25 15:30:58 -0400

    add function to fetch post

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -1,4 +1,4 @@
-"use server"
+"use server";
 
 import { revalidatePath } from "next/cache";
 
@@ -36,5 +36,39 @@ export async function createThread({
     revalidatePath(path);
   } catch (error: any) {
     throw new Error(`Error creating thread: ${error.message}`);
+  }
+}
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  try {
+    connectToDB();
+
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+      .sort({ createdAt: "desc" })
+      .skip(skipAmount)
+      .limit(pageSize)
+      .populate({ path: "author", model: User })
+      .populate({
+        path: "children",
+        populate: {
+          path: "author",
+          model: User,
+          select: "_id name parentId image",
+        },
+      });
+
+    const totalPostsCount = await Thread.countDocuments({
+      parentId: { $in: [null, undefined] },
+    });
+
+    const posts = await postsQuery.exec();
+
+    const isNext = totalPostsCount > skipAmount + posts.length;
+
+    return { posts, isNext };
+  } catch (error: any) {
+    throw new Error(`Failed to fetch post ${error.message}`);
   }
 }
