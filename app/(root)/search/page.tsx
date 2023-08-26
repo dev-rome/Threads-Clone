b331c38f61970d08f08841cd9d076b3703bcc655
dev-rome <haynesjerome@gmail.com>
2023-08-26 16:30:43 -0400

    add search bar

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,26 +1,35 @@
 import { redirect } from "next/navigation";
 import { currentUser } from "@clerk/nextjs";
 
-import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
 import UserCard from "@/components/cards/UserCard";
+import SearchBar from "@/components/SearchBar";
+import Pagination from "@/components/Pagination";
 
-export default async function page() {
-  const user = currentUser();
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
+  const user = await currentUser();
   if (!user) return null;
 
   const userInfo = await fetchUser(user.id);
-  if (!userInfo?.onboarded) redirect;
+  if (!userInfo?.onboarded) redirect("/onboarding");
 
   const result = await fetchUsers({
     userId: user.id,
-    searchString: "",
-    pageNumber: 1,
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
     pageSize: 25,
   });
 
   return (
     <section>
       <h1 className="head-text mb-10">Search</h1>
+
+      <SearchBar routeType="search" />
 
       <div className="mt-14 flex flex-col gap-9">
         {result.users.length === 0 ? (
@@ -40,6 +49,12 @@ export default async function page() {
           </>
         )}
       </div>
+
+      <Pagination
+        path="search"
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      />
     </section>
   );
 }
