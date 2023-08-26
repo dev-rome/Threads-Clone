03c7eee357d79a6f6cbda344f38c83cec71217e5
dev-rome <haynesjerome@gmail.com>
2023-08-26 12:53:52 -0400

    implement page to create thread with user information

--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -1,23 +1,22 @@
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
+import PostThread from "@/components/forms/PostThread";
 import { fetchUser } from "@/lib/actions/user.actions";
 
-import PostThread from "@/components/forms/PostThread";
-
-export default async function page() {
+export default async function Page() {
   const user = await currentUser();
-
   if (!user) return null;
 
+  // fetch organization list created by user
   const userInfo = await fetchUser(user.id);
-
   if (!userInfo?.onboarded) redirect("/onboarding");
+
   return (
-    <section>
+    <>
       <h1 className="head-text">Create Thread</h1>
 
       <PostThread userId={userInfo._id} />
-    </section>
+    </>
   );
 }
